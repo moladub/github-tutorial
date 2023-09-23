5996e8d3e7cc5d44ca6a54cacce9ecaa22719447
Sgun <your-segunadebayo721@gmail.com>
2023-09-23 01:33:56 +0100

    Edited main.py changed game rule and secret_number from (between 1 and 100) to ( between 1 and t0), also changed where all the word effort appeared to the previous word ATTEMPT

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# Original Segun Adebayo's game script
+# Original Segun's game script
 import random
 
-def play_game(): Segun Adebayo's first attempt
+def play_game(): Segun's first ATTEMPT
     print("Welcome to the Number Guessing Game!")
-    secret_number = random.randint(1, 50)
-    attempts = 0
+    secret_number = random.randint(1, 60)
+    ATTEMPTs = 0
 
     while True:
-        guess = int(input("Guess the secret number (between 1 and 50): "))
-        attempts += 1
+        guess = int(input("Guess the secret number (between 1 and 60): "))
+        ATTEMPTs += 1
 
         if guess < secret_number:
             print("Try higher!")
         elif guess > secret_number:
             print("Try lower!")
         else:
-            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            print(f"Congratulations! You guessed the number in {ATTEMPTs} ATTEMPTs.")
             break
 
 if__name__ == "__main__":
